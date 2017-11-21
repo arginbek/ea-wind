9c59db0f892c4fc9ef2228dc23ae474bfe682641
Oujeddi-Fatima <12934182+Oujeddi-Fatima@users.noreply.github.com>
2017-11-21 16:47:12 -0600

    Security done (#4)

--- ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/PersonServiceImpl.java
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/PersonServiceImpl.java
@@ -4,6 +4,7 @@ import edu.mum.cs544.wind.domain.Person;
 import edu.mum.cs544.wind.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.transaction.Transactional;
 import java.util.ArrayList;
@@ -12,13 +13,16 @@ import java.util.List;
 @Service
 @Transactional
 public class PersonServiceImpl implements PersonService {
-    @Autowired
-    PersonRepository personRepository;
+	@Autowired
+	PersonRepository personRepository;
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 
-    @Override
-    public Person addPerson(Person person) {
-        return personRepository.save(person);
-    }
+	@Override
+	public Person addPerson(Person person) {
+		person.setPassword(passwordEncoder.encode(person.getPassword()));
+		return personRepository.save(person);
+	}
 
     @Override
     public List<Person> getPersons() {
