b2de227405585fdb0c6cf3d75d8f7762e4bc6ede
arginbek <arginbek@gmail.com>
2017-11-22 11:27:46 -0600

    Revert "Testting pull request. (#9)" (#11)
    This reverts commit d7960b0d7d4b07f338e55bc3a0d357f59824208f.

--- ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/SessionServiceImpl.java
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/SessionServiceImpl.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Service
 @Transactional
 public class SessionServiceImpl implements SessionService {
-	  // Test
+
     @Autowired
     SessionRepository sessionRepository;
     @Autowired
