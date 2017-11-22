e4bdf04e9e0566b6290b6899b4f9798bd513951d
Son Pham <spham1412@gmail.com>
2017-11-21 18:31:10 -0600

    update datetime dependencies

--- ProofOfConcept/src/main/java/edu/mum/cs544/wind/domain/Role.java
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/domain/Role.java
@@ -1,5 +1,5 @@
 package edu.mum.cs544.wind.domain;
 
 public enum Role {
-    ADMIN, COUNSELOR, CUSTOMER;
+    ROLE_ADMIN, ROLE_COUNSELOR, ROLE_CUSTOMER;
 }
--- ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/PersonServiceImpl.java
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/service/PersonServiceImpl.java
@@ -3,8 +3,8 @@ package edu.mum.cs544.wind.service;
 import edu.mum.cs544.wind.domain.Person;
 import edu.mum.cs544.wind.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
@@ -12,16 +12,17 @@ import java.util.List;
 @Service
 @Transactional
 public class PersonServiceImpl implements PersonService {
-	@Autowired
-	PersonRepository personRepository;
-	@Autowired
-	private PasswordEncoder passwordEncoder;
+    @Autowired
+    PersonRepository personRepository;
 
-	@Override
-	public Person addPerson(Person person) {
-		person.setPassword(passwordEncoder.encode(person.getPassword()));
-		return personRepository.save(person);
-	}
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public Person addPerson(Person person) {
+        person.setPassword(passwordEncoder.encode(person.getPassword()));
+        return personRepository.save(person);
+    }
 
     @Override
     public List<Person> getPersons() {
