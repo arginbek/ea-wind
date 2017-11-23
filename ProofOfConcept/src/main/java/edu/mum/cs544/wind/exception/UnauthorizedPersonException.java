b0301993b2313309e8c0ab2985a6bc9f64f10bec
rafaelmum <rcosta@mum.edu>
2017-11-22 20:29:08 -0500

    Rafael (#17)
    * temporary work
    
    * Added Jackson dependency to handle JSON dates and some corrections.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * Security done
    
    * Removed hard code of Person in Session object
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * Added date validation to Session service on update operation.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * update datetime dependencies
    
    * temp
    
    * update new exceptions for update session
    
    * update new exceptions for update session
    
    * Added validation to Session use case and LocalDate converter.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * Fixed bug of not returning person when creating a session.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * Made Conselor mandatory on creating a session.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>
    
    * Updated authentication.
    
    Signed-off-by: rafaelmum <rcosta@mum.edu>

---
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/exception/UnauthorizedPersonException.java
@@ -0,0 +1,11 @@
+package edu.mum.cs544.wind.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value=HttpStatus.UNAUTHORIZED, reason="UNAUTHORIZED")
+public class UnauthorizedPersonException extends RuntimeException {
+    public UnauthorizedPersonException(String s) {
+        super(s);
+    }
+}
