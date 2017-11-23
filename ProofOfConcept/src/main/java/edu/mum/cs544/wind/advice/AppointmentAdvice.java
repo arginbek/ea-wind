9c5ce417aad506183b46f44432a747c59c336038
Oujeddi-Fatima <12934182+Oujeddi-Fatima@users.noreply.github.com>
2017-11-22 21:35:45 -0600

    Securitybranch (#23)
    * Security done
    
    * sending mail in progress
    
    * email sending in progress
    
    * email sending in progress
    
    * appointment services (#13)
    
    * # WARNING: head commit changed in the meantime
    
    conflict
    
    * Email service updated
    
    * email service done

--- ProofOfConcept/src/main/java/edu/mum/cs544/wind/advice/AppointmentAdvice.java
+++ ProofOfConcept/src/main/java/edu/mum/cs544/wind/advice/AppointmentAdvice.java
@@ -28,10 +28,12 @@ public class AppointmentAdvice {
 	private SessionRepository sessionRepository;
 
 	@AfterReturning(pointcut = "execution(* edu.mum.cs544.wind.service.AppointmentServiceImpl.addAppointment(..)) && args(personId, sessionId)", returning = "message")
-	public void notify(String message, Long personId, Long sessionId) {
+
+	public void notifyAppointmentCreation(String message, Long personId, Long sessionId) {
 		Person person = personRepository.findOne(personId);
 		Session session = sessionRepository.findOne(sessionId);
-		if (message.equals("You successfully booked")) {
+		if (message.equals("Booking Success: You have successfully booked an appointment.")) {
+
 			String counselorEmail = session.getCounselor().getEmail();
 			String customerEmail = person.getEmail();
 			String customerSuccessMessage = "Dear " + person.getFirstName() + ",\r\n\r\n"
@@ -51,14 +53,38 @@ public class AppointmentAdvice {
 
 			emailService.notifyByEmail(counselorEmail, message, customerSuccessMessage);
 			emailService.notifyByEmail(customerEmail, "New registration notification", counselorMessage);
-		} else {
-			String customerFailureMessage = " Hi " + person.getLastName() + ",\r\n"
-					+ "Our team thanks you for trying to register to our retreat session.\r\n"
-					+ "Infotunatly your this session is full."
+
+		} else if (message.equals("Booking Failed: Session is full, please choose another session.")) {
+			String customerFailureMessage = "Hi " + person.getLastName() + ",\r\n"
+					+ "Our team thanks you for trying to register to our retreat session."
+					+ "Infotunatly this session is full.\r\n"
+
 					+ "Please contact our customer service if you need more information windteamrocks@gmail.com!\r\n"
 					+ "\r\n" + "Best wishes,\r\n\r\n" + "Wind Team";
 
 			emailService.notifyByEmail(person.getEmail(), message, customerFailureMessage);
+		}
+	}
+
+
+	@AfterReturning(pointcut = "execution(* edu.mum.cs544.wind.service.AppointmentServiceImpl.removeAppointment(..)) && args(personId, sessionId, isAdmin)", returning = "message")
+	public void notifyAppointmentUpdate(String message, Long personId, Long sessionId, boolean isAdmin) {
+		Person person = personRepository.findOne(personId);
+		Session session = sessionRepository.findOne(sessionId);
+		if (message.equals("Cancel Booking Success: You have successfully cancel an appointment.")) {
+			String counselorEmail = session.getCounselor().getEmail();
+			String customerEmail = person.getEmail();
+			String customerSuccessMessage = "Dear " + person.getFirstName() + ",\r\n\r\n"
+					+ "Your booking of the session held by the counslor " + session.getCounselor().getFirstName() + " "
+					+ session.getCounselor().getLastName() + " on " + session.getDate() + "." + "\r\n" + "\r\n"
+					+ "Best wishes,\r\n\r\n" + "" + "Wind Team";
+			String counselorMessage = "Dear " + session.getCounselor().getFirstName() + ",\r\n\r\n"
+					+ "One appointment has been canceled for the session of " + session.getDate()
+					+ ". The current capacity of this session is : " + session.getCapacity() + ",\r\n\r\n"
+					+ "Wind Team";
+
+			emailService.notifyByEmail(counselorEmail, message, customerSuccessMessage);
+			emailService.notifyByEmail(customerEmail, "New registration notification", counselorMessage);
 		}
 	}
 
